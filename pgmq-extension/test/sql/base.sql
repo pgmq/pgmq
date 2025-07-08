70b8a4f746d0a3d6ea7a7a1ec857f29c387613a0
Adam Hendel <hendel.adam@gmail.com>
2025-07-08 09:17:21 -0500

    test queue and archive persist through extension drop

--- pgmq-extension/test/sql/base.sql
+++ pgmq-extension/test/sql/base.sql
@@ -286,9 +286,10 @@ SELECT tablename FROM pg_tables WHERE schemaname = 'pgmq' AND tablename = 'a_det
 
 -- queues and archive remains
 CREATE EXTENSION pgmq;
-SELECT pgmq.create('detach_archive_queue');
+SELECT pgmq.create('queue_remains');
 DROP EXTENSION pgmq CASCADE;
-SELECT tablename FROM pg_tables WHERE schemaname = 'pgmq' AND tablename = 'a_detach_archive_queue';
+SELECT tablename FROM pg_tables WHERE schemaname = 'pgmq' AND tablename = 'q_queue_remains';
+SELECT tablename FROM pg_tables WHERE schemaname = 'pgmq' AND tablename = 'a_queue_remains';
 
 --Truncated Index When queue name is max.
 CREATE EXTENSION pgmq;
