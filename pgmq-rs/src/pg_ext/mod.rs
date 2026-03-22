7793c65750e0424eec9df7b9e99f2529b741f5c7
Spencer Ferris <3319370+spencewenski@users.noreply.github.com>
2026-03-22 10:40:55 -0700

    Fix install doc path

--- pgmq-rs/src/pg_ext/mod.rs
+++ pgmq-rs/src/pg_ext/mod.rs
@@ -45,13 +45,13 @@ impl PGMQueueExt {
     }
 
     #[cfg(feature = "install-sql")]
-    #[doc = include_str!("./install/install_sql.md")]
+    #[doc = include_str!("../install/install_sql.md")]
     pub async fn install_sql_with_cxn(&self, pool: &Pool<Postgres>) -> Result<(), PgmqError> {
         crate::install::install_sql(pool).await
     }
 
     #[cfg(feature = "install-sql")]
-    #[doc = include_str!("./install/install_sql.md")]
+    #[doc = include_str!("../install/install_sql.md")]
     pub async fn install_sql(&self) -> Result<(), PgmqError> {
         self.install_sql_with_cxn(&self.connection).await
     }
