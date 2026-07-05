a16046dae4e300a903bd98153868cd82aa99af69
Spencer Ferris <3319370+spencewenski@users.noreply.github.com>
2026-07-05 01:07:56 -0700

    Fix `handle_read_batch_result` types required due to merge from main

--- pgmq-rs/src/private/util.rs
+++ pgmq-rs/src/private/util.rs
@@ -1,12 +1,16 @@
 /// Helper method to convert [`PgRow`] to [`Message`] for the `read*`/`read_batch*` methods.
 #[cfg(feature = "sqlx")]
-pub fn handle_read_batch_result<T: for<'de> serde::Deserialize<'de>>(
+pub fn handle_read_batch_result<T, H>(
     rows: Vec<sqlx::postgres::PgRow>,
-) -> Result<Vec<crate::types::Message<T>>, crate::errors::PgmqError> {
+) -> Result<Vec<crate::types::Message<T, H>>, crate::errors::PgmqError>
+where
+    T: for<'de> serde::Deserialize<'de>,
+    H: for<'de> serde::Deserialize<'de>,
+{
     use sqlx::FromRow;
     let messages = rows
         .into_iter()
-        .map(|row| crate::types::Message::<T>::from_row(&row))
-        .collect::<Result<Vec<crate::types::Message<T>>, _>>()?;
+        .map(|row| crate::types::Message::<T, H>::from_row(&row))
+        .collect::<Result<Vec<crate::types::Message<T, H>>, _>>()?;
     Ok(messages)
 }
